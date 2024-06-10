aea67a5271ecbad6cfd33deef7a7b7102768e4f7
Kohei ASAI <asai-kohei@g.ecc.u-tokyo.ac.jp>
2024-06-10 17:33:22 +0900

    add training job file

---
+++ youtube_u2s_training.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#PJM -L rscgrp=short-a
+#PJM -L node=1
+#PJM -g ge43
+#PJM -j
+set -x
+source /etc/profile.d/modules.sh
+module load gcc/12.2.0
+module load cuda/12.1
+source .venv/bin/activate
+
+ratarmount ~/fairseq/corpus/hq-youtube.tar /dev/shm/hq-youtube_mount
+cd examples
+HYDRA_FULL_ERROR=1 python3 train_u2s.py u2s=xvector u2s/datamodule=wisteria_xvector.yaml
